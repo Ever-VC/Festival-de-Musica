2148af5abb372d5b49e9c7c34d54d07c048dbe20
Ever-VC <ever.vasquezc27@gmail.com>
2024-12-22 17:22:04 -0600

    💄 UI:  Evitando scroll al visualizar una imagen y botón de cerrar

--- src/js/app.js
+++ src/js/app.js
@@ -9,13 +9,60 @@ function crearGaleria() {
     const CANTIDAD_IMAGENES = 16;
     const URL_IMAGENES = 'src/img/gallery/full/';
     for(let i = 1; i <= CANTIDAD_IMAGENES; i++) {
+        // Crear imagen
         const imagen = document.createElement('IMG');
         imagen.src = `${URL_IMAGENES}${i}.jpg`;
         imagen.alt = 'Imagen Galeria';
+
+        // Agregar evento click a la imagen (Event Handler) para mostrar la imagen en el modal
+        imagen.onclick = function() {
+            mostrarImagen(imagen);
+        }
+
+        // Agregar imagen a la galeria
         galeria.appendChild(imagen);
     }
 }
 
-const obtenerURLImagen = (imagen) => {
-    return imagen.src;
+function mostrarImagen(imagen) {
+    // Crear modal
+    const modal = document.createElement('DIV');
+    modal.classList.add('modal');
+
+    // Evento click para cerrar el modal
+    modal.onclick = cerrarModal;
+
+    // Agregar boton para cerrar el modal
+    const btnCerrar = document.createElement('BUTTON');
+    btnCerrar.textContent = 'X';
+    btnCerrar.classList.add('btn-cerrar');
+    btnCerrar.onclick = cerrarModal;
+
+    // Crear imagen en el modal
+    const imagenModal = document.createElement('IMG');
+    imagenModal.src = imagen.src;
+    imagenModal.alt = imagen.alt;
+
+    // Agregar imagen al modal
+    modal.appendChild(imagenModal);
+
+    // Agregar boton al modal
+    modal.appendChild(btnCerrar);
+
+    // Agregal al body
+    const body = document.querySelector('body');
+    body.classList.add('overflow-hidden');
+    body.appendChild(modal);
+}
+
+function cerrarModal(e) {
+    const modal = document.querySelector('.modal');
+    
+    modal.classList.add('fade-out');
+    setTimeout(() => {
+        modal?.remove();
+        const body = document.querySelector('body');
+        body.classList.remove('overflow-hidden');
+    }, 500);
+    
 }
