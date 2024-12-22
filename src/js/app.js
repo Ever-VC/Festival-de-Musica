e63b0484a171fb926ef20eefab3845018ee73314
Ever-VC <ever.vasquezc27@gmail.com>
2024-12-22 16:20:51 -0600

    ✨ New Feature: Agregando la galería de imágenes con js

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,21 @@
-alert('Desde app.js')
+document.addEventListener('DOMContentLoaded', function() {
+    crearGaleria();
+});
+
+function crearGaleria() {
+    const galeria = document.querySelector('.galeria-imagenes');
+
+    // Crear 16 imagenes y agregarlas a la galeria
+    const CANTIDAD_IMAGENES = 16;
+    const URL_IMAGENES = 'src/img/gallery/full/';
+    for(let i = 1; i <= CANTIDAD_IMAGENES; i++) {
+        const imagen = document.createElement('IMG');
+        imagen.src = `${URL_IMAGENES}${i}.jpg`;
+        imagen.alt = 'Imagen Galeria';
+        galeria.appendChild(imagen);
+    }
+}
+
+const obtenerURLImagen = (imagen) => {
+    return imagen.src;
+}
